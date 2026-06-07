17d616efe38edd90cc82adbf343378baf7fda2b7
Dominique9325 <banicdominik256@gmail.com>
2026-06-07 21:05:31 +0200

    added previously untracked file

--- common/netwrap.h
+++ common/netwrap.h
@@ -6,7 +6,7 @@
 #define LWMSG_SOCKETWRAP_H
 #include <stdbool.h>
 #include <stdint.h>
-#define ERROR -1
+#define ERROR (-1)
 
 int32_t server_start_tcp(uint32_t inet4addr, uint16_t port, uint8_t backlog);
 
